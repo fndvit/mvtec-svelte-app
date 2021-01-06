f3e1ef79e592e0001741bea19f11d014f74bcd66
xaquingv <xocasgv@gmail.com>
2021-01-06 12:54:30 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import App from './App.svelte';
+
+const url = 'URL to your JSON data';
+
+fetch(url)
+  .then((res) => res.json())
+  .then((json) => {
+	const data = json;
+
+    const app = new App({
+      target: document.body,
+      props: {
+		data: data
+		/*You can also pass each object in 'data' as an individual prop.
+		For example:
+			meta: data.meta,
+			menu: data.menu,
+			content: data.content
+		*/
+      }
+	});
+});
+
+export default app;
+
+
